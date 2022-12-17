9b43beba0655b18fc7fb7fa883a98f0850bc2467
Norman Alié <mail@normanalie.fr>
2022-12-17 17:05:30 +0000

    Display snake

--- .ccls-cache/@home@runner@snake/main.c
+++ .ccls-cache/@home@runner@snake/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Afficher board
+// Afficher board OK
 // Afficher serpent L=3
 // Avancer serpent
 // Diriger serpent
@@ -11,34 +12,82 @@
 // Afficher game over
 // Afficher menu
 
-#define BOARD_SIZE 15
+#define BOARD_HEIGHT 15
+#define BOARD_WIDTH 22
 #define BOARD_WALL 'X'
+#define SNAKE_HEAD '@'
+#define SNAKE_BODY '*'
 
-void print_board(int size, char wall);
+typedef struct Body Body;
+struct Body {
+  int x;
+  int y;
+  int is_head;
+  Body *next;
+};
+
+typedef struct {
+  int len;
+  int direction;
+  int score;
+  Body *head;
+} Snake;
+
+void init_snake(Snake *snake, int x, int y);
+int append_snake(Snake *snake);
+void display(int width, int height, char wall, Snake *snake);
 
 int main(void) {
-  print_board(BOARD_SIZE, BOARD_WALL);
+  Snake snake;
+  init_snake(&snake, 2, 10);
+  display(BOARD_WIDTH, BOARD_HEIGHT, BOARD_WALL, &snake);
   return 0;
 }
 
-void print_board(int size, char wall) {
-  float ratio = 1.5; // line/column spacement ratio to have a square board
-  int width = (int)size * 1.5;
-  // Top line
-  for (int i = 0; i < width + 1; i++) {
-    printf("%c", wall);
+void init_snake(Snake *snake, int x, int y) {
+  snake->len = 1;
+  snake->direction = 1;
+  snake->score = 0;
+  Body *head = malloc(sizeof(*head));
+  if (head == NULL) {
+    exit(EXIT_FAILURE);
   }
-  printf("%c\n", wall);
+  head->x = x;
+  head->y = y;
+  head->is_head = 1;
+  head->next = NULL;
+  snake->head = head;
+  return;
+}
 
-  // Sides
-  for (int i = 0; i < size - 2; i++) { // size-(TopLine+BottomLine)
-    printf("%c %*c\n", wall, width, wall);
-  }
+int append_snake(Snake *snake) { return 0; }
 
-  // Bottom line
-  for (int i = 0; i < width + 1; i++) {
-    printf("%c", wall);
+void display(int width, int height, char wall, Snake *snake) {
+  for (int i = 0; i <= height; i++) {
+    for (int j = 0; j <= width; j++) {
+      // WALLS
+      if (i == 0 || i == height) {
+        printf("%c", wall);
+      } else if (j == 0 || j == width) {
+        printf("%c", wall);
+      } else {
+        Body *snake_body = snake->head;
+        while (snake_body != NULL) {
+          if (snake_body->x == j && snake_body->y == i) {
+            printf("%c", snake_body->is_head == 1 ? SNAKE_HEAD : SNAKE_BODY);
+          } else {
+            printf(" ");
+          }
+          snake_body = snake_body->next;
+        }
+      }
+      // INSIDE
+
+      // EOL
+      if (j == width) {
+        printf("\n");
+      }
+    }
   }
-  printf("%c\n", wall);
   return;
 }
