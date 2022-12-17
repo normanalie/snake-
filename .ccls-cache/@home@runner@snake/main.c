901e5e37ba9778f940ac8622ec14bd72c173d09d
ZeProf2C <>
2022-12-17 15:01:35 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@snake/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Afficher board
+// Afficher serpent L=3
+// Avancer serpent
+// Diriger serpent
+// Collision serpent
+// Afficher fruit
+// Grandir serpent
+// Generer fruit
+// Afficher game over
+// Afficher menu
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@snake/test.c
@@ -0,0 +1,25 @@
+#include "gestion_clavier.h"
+#include <stdio.h>
+
+#define UP 'z'
+#define DOWN 's'
+#define LEFT 'q'
+#define RIGHT 'd'
+#define ESC 27
+
+int main() {
+  char key;
+
+  do {
+    key = pressing_key();
+    if (key == NO_KEY) {
+      printf("Aucune touche\n");
+    } else {
+      printf("La touche %c a été appuyée...\n", key);
+    }
+
+  } while (key != 27);
+
+  printf("##\nle programme est terminé.\n");
+  return 0;
+}
