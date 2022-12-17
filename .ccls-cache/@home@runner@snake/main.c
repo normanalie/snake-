91429ded97fccbbe1807f0d47553c153393e794c
Norman Alié <mail@normanalie.fr>
2022-12-17 15:21:48 +0000

    Print board

--- .ccls-cache/@home@runner@snake/main.c
+++ .ccls-cache/@home@runner@snake/main.c
@@ -11,7 +11,34 @@
 // Afficher game over
 // Afficher menu
 
+#define BOARD_SIZE 15
+#define BOARD_WALL 'X'
+
+void print_board(int size, char wall);
+
 int main(void) {
-  printf("Hello World\n");
+  print_board(BOARD_SIZE, BOARD_WALL);
   return 0;
+}
+
+void print_board(int size, char wall) {
+  float ratio = 1.5; // line/column spacement ratio to have a square board
+  int width = (int)size * 1.5;
+  // Top line
+  for (int i = 0; i < width + 1; i++) {
+    printf("%c", wall);
+  }
+  printf("%c\n", wall);
+
+  // Sides
+  for (int i = 0; i < size - 2; i++) { // size-(TopLine+BottomLine)
+    printf("%c %*c\n", wall, width, wall);
+  }
+
+  // Bottom line
+  for (int i = 0; i < width + 1; i++) {
+    printf("%c", wall);
+  }
+  printf("%c\n", wall);
+  return;
 }
